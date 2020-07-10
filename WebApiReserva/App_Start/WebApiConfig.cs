6f62402f1d78ef8f5170fb540a24b34980f96887
arielangeles <arielangeles123@hotmail.com>
2020-07-10 12:45:29 -0400

    Only MapByAction Route configured

--- WebApiReserva/App_Start/WebApiConfig.cs
+++ WebApiReserva/App_Start/WebApiConfig.cs
@@ -22,11 +22,6 @@ namespace WebApiReserva
                routeTemplate: "api/{controller}/{action}/{id}",
                defaults: new { id = RouteParameter.Optional });
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional });
-
             config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
         }
     }
