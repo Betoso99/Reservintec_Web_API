b6086a05af313466ac5e0d2d3cec057395049b74
arielangeles <arielangeles123@hotmail.com>
2020-07-12 22:21:08 -0400

    pk

--- WebApiReserva/Controllers/EdificioController.cs
+++ WebApiReserva/Controllers/EdificioController.cs
@@ -147,13 +147,13 @@ namespace WebApiReserva.Controllers
                 return Ok(log);
             }
 
-            //if (cursos.Count > 0)
-            //{
-            //    log.Ok = false;
-            //    log.ErrorMessage = "Lista sin filtro";
-            //    var res = MergeLogResult(log, cursos);
-            //    return Ok(res);
-            //}
+            if (cursos.Count > 0)
+            {
+                log.Ok = false;
+                log.ErrorMessage = "Lista sin filtro";
+                var res = MergeLogResult(log, cursos);
+                return Ok(res);
+            }
 
             List<CursoEdificio> listaResult = new List<CursoEdificio>();
             int cantidadEdificios = db.tblEdificio.Select(e => e.idEdificio).ToList().Count();
